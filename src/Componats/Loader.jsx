cc13fa1304c1a0e5b7be71eba91b48664a9c3918
Priyank123p <psvagadia2003@gmail.com>
2026-01-21 17:43:38 +0530

    Commit

--- src/Componats/Loader.jsx
+++ src/Componats/Loader.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import './Loader.css';
-
+import CricketerImg from '../Img/Cricketer-IMG/Loading-Bat.png';
 const Loader = () => {
     return (
         <div className="loader-container">
@@ -19,29 +19,7 @@ const Loader = () => {
                     }}
                 >
                     {/* Simple SVG Cricket Bat */}
-                    <svg width="100" height="250" viewBox="0 0 100 250" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        {/* Handle */}
-                        <rect x="42" y="0" width="16" height="70" rx="4" fill="#5D4037" />
-                        <rect x="42" y="10" width="16" height="50" rx="4" fill="url(#handleGradient)" />
-
-                        {/* Blade */}
-                        <path d="M20 70 H80 L85 240 Q85 250 50 250 Q15 250 15 240 L20 70 Z" fill="#D7CCC8" stroke="#8D6E63" strokeWidth="2" />
-
-                        {/* Spine (giving it 3D look) */}
-                        <path d="M50 70 L50 250" stroke="#BCAAA4" strokeWidth="1" />
-
-                        {/* Stickers/Details */}
-                        <rect x="25" y="150" width="50" height="40" fill="#ff481fff" opacity="0.9" />
-                        <text x="50" y="175" fontSize="10" fill="white" textAnchor="middle" fontWeight="bold" style={{ fontFamily: 'Arial' }}>JM</text>
-
-                        <defs>
-                            <linearGradient id="handleGradient" x1="50" y1="10" x2="50" y2="60" gradientUnits="userSpaceOnUse">
-                                <stop stopColor="#5D4037" />
-                                <stop offset="0.5" stopColor="#8D6E63" />
-                                <stop offset="1" stopColor="#5D4037" />
-                            </linearGradient>
-                        </defs>
-                    </svg>
+                        <img src={CricketerImg} alt="loading bat" />
                 </motion.div>
 
                 {/* Text Container */}
